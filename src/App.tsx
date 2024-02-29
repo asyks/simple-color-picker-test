547e07932dc7f3accdcd7d96c58a099055e16566
asyks <aaron@aaronsykes.com>
2024-02-28 16:27:18 -0800

    Adjust rgb selection to 255 scale

--- src/App.tsx
+++ src/App.tsx
@@ -17,20 +17,19 @@ function pad2(c: string): string {
 }
 
 function hexNumberToRgb(color: number): RGBAColor {
-  const r = ((color >> 16) & 255) / 255
-  const g = ((color >> 8) & 255) / 255
-  const b = (color & 255) / 255
+  const r = ((color >> 16) & 255)
+  const g = ((color >> 8) & 255)
+  const b = (color & 255)
   return { r, g, b }
 }
 
 function rgbToHex(color: RGBAColor): string {
-  console.log(color)
   const { r, g, b } = color
   var hex = [
     '#',
-    pad2(Math.round(r * 255).toString(16)),
-    pad2(Math.round(g * 255).toString(16)),
-    pad2(Math.round(b * 255).toString(16))
+    pad2(Math.abs(r).toString(16)),
+    pad2(Math.abs(g).toString(16)),
+    pad2(Math.abs(b).toString(16))
   ]
   console.log(hex)
 
@@ -40,9 +39,9 @@ function rgbToHex(color: RGBAColor): string {
 
 const App: React.FC = () => {
   const [colorHex, setColorHex] = useState('#ffffff')
-  const [colorRed, setColorRed] = useState(0.00)
-  const [colorGreen, setColorGreen] = useState(0.00)
-  const [colorBlue, setColorBlue] = useState(0.00)
+  const [colorRed, setColorRed] = useState(0)
+  const [colorGreen, setColorGreen] = useState(0)
+  const [colorBlue, setColorBlue] = useState(0)
 
   const colorPicker = useMemo(() => new ColorPicker({
     el: document.getElementById('color-picker')!,
@@ -70,21 +69,21 @@ const App: React.FC = () => {
     setColorRed(colorNum)
     const hex = rgbToHex({r: colorNum, g: colorGreen, b: colorBlue})
     colorPicker.setColor(hex)
-    setColorHex(hex)
+    document.body.style.background = colorHex
   }, [colorGreen, colorBlue])
 
   const setColorGreenCallback = useCallback((colorNum: number) => {
     setColorGreen(colorNum)
     const hex = rgbToHex({r: colorRed, g: colorNum, b: colorBlue})
     colorPicker.setColor(hex)
-    setColorHex(hex)
+    document.body.style.background = colorHex
   }, [colorRed, colorBlue])
 
   const setColorBlueCallback = useCallback((colorNum: number) => {
     setColorBlue(colorNum)
     const hex = rgbToHex({r: colorRed, g: colorGreen, b: colorNum})
     colorPicker.setColor(hex)
-    setColorHex(hex)
+    document.body.style.background = colorHex
   }, [colorRed, colorGreen])
 
   return (
@@ -93,15 +92,15 @@ const App: React.FC = () => {
         Pick a Color
       </header>
       <label>r:</label>
-      <input type="number" step="0.0001" min="0.00" max="1.00" value={colorRed} onChange={
+      <input type="number" step="1" min="0" max="255" value={colorRed} onChange={
         (e) => e.target.value && setColorRedCallback(parseInt(e.target.value))
       }/>
       <label>g:</label>
-      <input type="number" step="0.0001" min="0.00" max="1.00" value={colorGreen} onChange={
+      <input type="number" step="1" min="0" max="255" value={colorGreen} onChange={
         (e) => e.target.value && setColorGreenCallback(parseInt(e.target.value))
       }/>
       <label>b:</label>
-      <input type="number" step="0.0001" min="0.00" max="1.00" value={colorBlue} onChange={
+      <input type="number" step="1" min="0" max="255" value={colorBlue} onChange={
         (e) => e.target.value && setColorBlueCallback(parseInt(e.target.value))
       }/>
     </div>
